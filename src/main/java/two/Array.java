2672a4d3f24ac15391ec75183859881ce189fabf
sugare <30733705@qq.com>
2019-03-18 16:06:07 +0800

    feat(*): add complexity

--- src/main/java/two/Array.java
+++ src/main/java/two/Array.java
@@ -1,5 +1,11 @@
 package two;
 
+/*
+* 增: O(n)
+* 删: O(n)
+* 改: 已知索引 O(1)，未知索引 O(n)
+* 查: 已知索引 O(1)，未知索引 O(n)
+* */
 public class Array<E> {
 
     private E[] data;
@@ -26,6 +32,17 @@ public class Array<E> {
         return data.length;
     }
 
+    // O(n)
+    private void resize(int capacity) {
+
+        E[] newData = (E[]) new Object[capacity];
+        for (int i = 0; i< size; i++)
+            newData[i] = data[i];
+
+        data = newData;
+    }
+
+    // O(n/2) = O(n)
     public void add(int index, E e){
 
         if (index <0 || index > size)
@@ -43,23 +60,17 @@ public class Array<E> {
         size ++;
     }
 
-    private void resize(int capacity) {
-
-        E[] newData = (E[]) new Object[capacity];
-        for (int i = 0; i< size; i++)
-            newData[i] = data[i];
-
-        data = newData;
-    }
-
+    // O(n)
     public void addFirst(E e){
         add(0, e);
     }
 
+    // O(1)
     public void addLast(E e){
         add(size, e);
     }
 
+    // O(n)
     public boolean contains(E e){
         for (int i=0;i<size;i++){
             return data[i] == e;
@@ -67,6 +78,7 @@ public class Array<E> {
         return false;
     }
 
+    // O(n)
     public int find(E e){
         for (int i=0;i<size;i++){
             if (data[i] == e)
@@ -75,6 +87,7 @@ public class Array<E> {
         return -1;
     }
 
+    // O(n/2) = O(n)
     public E remove(int index){
 
         if (index <0 || index > size)
@@ -87,20 +100,23 @@ public class Array<E> {
         }
         size--;
 
-        if (size == data.length/2)
-            resize(data.length/2);
+        if (size == data.length / 4  && data.length / 2 != 0)
+            resize(data.length / 2);
 
         return temp;
     }
 
+    // O(n)
     public E removeFirst(){
         return  remove(0);
     }
 
+    // O(1)
     public E removeLast(){
-        return  remove(size-1);
+        return  remove( -1);
     }
 
+    // O(n)
     public void removeElement(E e){
         int index = find(e);
         if (index != -1){
@@ -108,12 +124,14 @@ public class Array<E> {
         }
     }
 
+    // O(1)
     public E get(int index){
         if (index <0 || index > size)
             throw new IllegalArgumentException("index error");
         return data[index];
     }
 
+    // O(1)
     public void set(int index, E e){
         data[index] = e;
     }
